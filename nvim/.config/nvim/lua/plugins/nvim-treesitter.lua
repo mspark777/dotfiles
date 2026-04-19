c6d66e71e293bb00f8ec670adca7e70d67b4f788
mspark777 <topangel777@gmail.com>
2026-04-19 23:18:24 +0900

    Upgarde

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,15 +2,4 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
-	otps = {
-		modules = {},
-		sync_install = false,
-		ensure_installed = {},
-		ignore_install = {},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-	},
 }
